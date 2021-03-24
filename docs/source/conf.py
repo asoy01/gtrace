37260c6733148a17733a7d47a90baf51587f348b
terrencetec <terrencetec@gmail.com>
2021-03-24 22:34:00 +0800

    Changed docs file structure so make html works for building local documentations

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,4 +54,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autoapi_dirs = ['../gtrace']
+autoapi_dirs = ['../../gtrace']
