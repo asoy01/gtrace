a1223620a0cc0d37424554542530d6b65a4559d5
Yoichi Aso <asoy01@gmail.com>
2025-03-07 17:10:19 +0900

    Increased the version to 0.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gtrace",
-    version="0.2.3",
+    version="0.2.4",
     author="Yoichi Aso",
     author_email="yoichi.aso@nao.ac.jp",
     description="2D ray tracing package for Gaussian beams",
